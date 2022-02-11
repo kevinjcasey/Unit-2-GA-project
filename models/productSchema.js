4a6aa030718f43d50556ed731a68d0e0ac3676c2
Kevin Casey <kevinjcasey@kevins-air-m1.myfiosgateway.com>
2022-02-11 12:41:06 -0500

    nothing is working. Awesome.

---
+++ models/productSchema.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    price: {type: Number, required: true},
+    image: String,
+    description: String,
+    category: String,
+    purchased: Boolean
+})
+
+const productCollection = mongoose.model('Products', productSchema);
+
+module.exports = productCollection
+
---
+++ models/products.js
@@ -0,0 +1,12 @@
+const products = [
+    {
+        name: 'Coupe Glass',
+        price: 15,
+        image: '',
+        description: 'Beautful hand-blown coupe glass',
+        category: 'Glassware',
+        purchased: false
+    }
+]
+
+module.exports = products
