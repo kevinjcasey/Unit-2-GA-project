629ec72094b870024db3a692859f972bb48daf67
Kevin Casey <kevinjcasey@kevins-air-m1.myfiosgateway.com>
2022-02-11 20:28:09 -0500

    Added products to database

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Unit-2-GA-project
+
+Products database:
+
+Categories -- 
+    -Drinkware
+        -Martini
+        -Rocks
+        -Coupe - Leopold
+        -Highball
+        -Hurricane
+        -Tiki mug
+    -Bar essentials: 
+        -Bar spoons
+        -Jiggers
+        -Mixers
+        -Strainers
+    -Ice stuff
+        -Stamp
+        -Molds
+        -Something cool
+    -Accessories
+        -Peeler
+        -Juicer
+        -Roll-up kit
+    -Aprons/kits
+    -Books
+    -Bitters
--- models/products.js
+++ models/products.js
@@ -1,12 +1,204 @@
 const products = [
+
+// ===== Glassware ====== //
+
     {
-        name: 'Coupe Glass',
-        price: 15,
-        // image: '',
-        description: 'Beautful hand-blown coupe glass',
+        name: 'Coupe',
+        price: 8,
+        image: '',
+        description: 'Vestibulum auctor dapibus neque',
         category: 'Glassware',
         purchased: false
-    }
+    },
+    {
+        name: 'Martini/Cocktail',
+        price: 8,
+        image: '',
+        description: 'Vestibulum auctor dapibus neque',
+        category: 'Glassware',
+        purchased: false
+    },
+    {
+        name: 'Old-fashioned',
+        price: 10,
+        image: '',
+        description: 'Vestibulum auctor dapibus neque',
+        category: 'Glassware',
+        purchased: false
+    },
+    {
+        name: '',
+        price: '',
+        image: '',
+        description: 'Vestibulum auctor dapibus neque',
+        category: 'Glassware',
+        purchased: false
+    },
+    {
+        name: '',
+        price: '',
+        image: '',
+        description: 'Vestibulum auctor dapibus neque',
+        category: 'Glassware',
+        purchased: false
+    },
+    {
+        name: '',
+        price: '',
+        image: '',
+        description: 'Vestibulum auctor dapibus neque',
+        category: 'Glassware',
+        purchased: false
+    },
+
+// ====== Bar essentials ====== //
+
+{
+    name: '',
+    price: '',
+    image: '',
+    description: 'Nunc dignissim risus id metus',
+    category: '',
+    purchased: false
+},
+{
+    name: '',
+    price: '',
+    image: '',
+    description: 'Nunc dignissim risus id metus',
+    category: '',
+    purchased: false
+},
+{
+    name: '',
+    price: '',
+    image: '',
+    description: 'Nunc dignissim risus id metus',
+    category: '',
+    purchased: false
+},
+{
+    name: '',
+    price: '',
+    image: '',
+    description: 'Nunc dignissim risus id metus',
+    category: '',
+    purchased: false
+},
+{
+    name: '',
+    price: '',
+    image: '',
+    description: 'Nunc dignissim risus id metus',
+    category: '',
+    purchased: false
+},
+{
+    name: '',
+    price: '',
+    image: '',
+    description: 'Nunc dignissim risus id metus',
+    category: '',
+    purchased: false
+},
+{
+    name: '',
+    price: '',
+    image: '',
+    description: 'Nunc dignissim risus id metus',
+    category: '',
+    purchased: false
+},
+{
+    name: '',
+    price: '',
+    image: '',
+    description: 'Nunc dignissim risus id metus',
+    category: '',
+    purchased: false
+},
+
+// ====== Ice stuff ===== //
+
+{
+    name: '',
+    price: '',
+    image: '',
+    description: 'Praesent placerat risus quis eros',
+    category: '',
+    purchased: false
+},
+{
+    name: '',
+    price: '',
+    image: '',
+    description: 'Praesent placerat risus quis eros',
+    category: '',
+    purchased: false
+},
+{
+    name: '',
+    price: '',
+    image: '',
+    description: 'Praesent placerat risus quis eros',
+    category: '',
+    purchased: false
+},
+{
+    name: '',
+    price: '',
+    image: '',
+    description: 'Praesent placerat risus quis eros',
+    category: '',
+    purchased: false
+},
+
+// ====== Accessories ====== //
+
+{
+    name: '',
+    price: '',
+    image: '',
+    description: 'Integer vitae libero ac risus egestas placerat',
+    category: '',
+    purchased: false
+},
+{
+    name: '',
+    price: '',
+    image: '',
+    description: 'Integer vitae libero ac risus egestas placerat',
+    category: '',
+    purchased: false
+},
+{
+    name: '',
+    price: '',
+    image: '',
+    description: 'Integer vitae libero ac risus egestas placerat',
+    category: '',
+    purchased: false
+},
+{
+    name: '',
+    price: '',
+    image: '',
+    description: 'Integer vitae libero ac risus egestas placerat',
+    category: '',
+    purchased: false
+},
+{
+    name: '',
+    price: '',
+    image: '',
+    description: 'Integer vitae libero ac risus egestas placerat',
+    category: '',
+    purchased: false
+},
+
+// ======== Books ======== //
+
+// ======== Bitters ======== //
 ]
 
 module.exports = products
